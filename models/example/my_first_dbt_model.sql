1b43f84acd4a6540c73746bb7941c9608ddf2d95
rahulnathjogi1997@gmail.com <rahulnathjogi1997@gmail.com>
2023-10-19 09:28:39 +0000

    Simform word added

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,5 +1,5 @@
-
 /*
+    Simform
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
